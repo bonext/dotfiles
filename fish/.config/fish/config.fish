df78e0e22201b432d5a783722e13b22a58b5144f
Alexander Molchanov <bonext@mailbox.org>
2025-03-26 22:11:05 +0100

    (fish): update config
    - ignore some files
    - update irc abbr
    - integrate direnv
    - integrate fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,12 @@
-fish_add_path ~/.local/bin
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # https://direnv.net/docs/hook.html#fish
+    direnv hook fish | source
+
+    # https://junegunn.github.io/fzf/shell-integration/#setting-up-shell-integration
+    fzf --fish | source
+    
+    # abbr gets expanded to a full command before execution
+    abbr -a irc tmux new-session -A -s irc weechat
 end
---
+++ fish/.config/fish/functions/show_user_paths.fish
@@ -0,0 +1,3 @@
+function show_user_paths
+    echo $fish_user_paths | tr " " "\n" | nl
+end
